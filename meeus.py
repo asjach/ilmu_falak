7d67565efc24faf616ec759d5ffd1066b0c75ac4
asjach <asjach@gmail.com>
2022-12-23 23:51:39 +0700

    persiapan perhitungan data bulan

--- meeus.py
+++ meeus.py
@@ -71,7 +71,6 @@ class DetailPerhitungan:
         self.menit = menit
         self.detik = detik
         self.timezone = timezone
-
         _m = self.bulan + 12 if self.bulan < 3 else self.bulan
         _y = self.tahun - 1 if self.bulan < 3 else self.tahun
         _a = int(_y/100)
@@ -91,15 +90,15 @@ class DetailPerhitungan:
         _t_ut            = (jd_ut-2451545)/36525
         _delta           = delta_t(tgl.tahun_desimal)
         _jde             = jd_ut + _delta/86400
-        _t_td            = (_jde - 2451545)/36525
-        _tau             = _t_td/10
+        self.t_td            = (_jde - 2451545)/36525
+        _tau             = self.t_td/10
         _gst             = ((280.46061837 + 360.98564736629*(jd_ut - 2451545) + 0.000387933*_t_ut**2 - _t_ut**3/38710000) % 360)/15
-        _delta_e         = delta_epsilon_total(_t_td)
-        _u               = u(_t_td)
+        _delta_e         = delta_epsilon_total(self.t_td)
+        _u               = u(self.t_td)
         _epsilon_z       = epsilon_zero(_u)
-        _delta_e_total   = delta_epsilon_total(_t_td)/10000
+        _delta_e_total   = delta_epsilon_total(self.t_td)/10000
         _epsilon         = math.radians(epsilon(_epsilon_z, _delta_e_total))
-        _delta_psi_total = delta_psi_total(_t_td)
+        _delta_psi_total = delta_psi_total(self.t_td)
         _delta_psi       = _delta_psi_total/3600
         _gst_nampak      = _gst + _delta_psi * math.cos(_epsilon)/15
         _lst_nampak      = (_gst_nampak + bandung.bujur()/15) % 24
@@ -110,7 +109,17 @@ class DataMatahari:
 
 
 class DataBulan:
-    pass
+    def __init__ (self, t_td):
+        self.t_td       = t_td
+        l_aksen         = (218.3164591 + 481267.88134236*self.t_td - 0.0013268*self.t_td*self.t_td + self.t_td*self.t_td*self.t_td/538841 - self.t_td*self.t_td*self.t_td*self.t_td/65194000) % 360
+        print(l_aksen)
+        koreksi_bujur_bulan = 0
+
+
+
+
+            
+
 #test
 
 
@@ -118,4 +127,7 @@ bandung = Koordinat(6,57,0,"LS", 107, 37,0,"BT")
 #tgl = Tanggal(14, 7, 2016)   
 tgl = Tanggal(14, 10, 2022)   
 pukul = Waktu(9,0,0,7)
-detail_perhitungan = DetailPerhitungan(tgl.tahun, tgl.bulan, tgl.hari, pukul.jam, pukul.menit, pukul.detik, pukul.timezone)
+calc = DetailPerhitungan(tgl.tahun, tgl.bulan, tgl.hari, pukul.jam, pukul.menit, pukul.detik, pukul.timezone)
+bulan =DataBulan(calc.t_td)
+
+#print(to_dms(xxxxx))
