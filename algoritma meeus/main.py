cfac946001567661e046c5cdd0229b157e82dd39
asjach <asjach@gmail.com>
2022-12-26 17:36:58 +0700

    refaktor class moon menjadi moon dan moon correction

--- algoritma meeus/main.py
+++ algoritma meeus/main.py
@@ -3,10 +3,11 @@ from utilities import *
 from calcutation import *
 from place import Place
 #from functions.nutation import Nutation
-from moon import *
+# from moon import *
 from sun import *
 from nutation import *
 from delta_t import delta_T
+from moon_correction import *
 
 
 class Tanggal:
@@ -88,50 +89,61 @@ lokasi1 = Place("Bandung", 6,57,0,"LS", 107, 37,0,"BT")
 tanggal = Tanggal(2022,12,14)
 waktu = Waktu(14, 0, 0, 7)
 T = KonversiWaktu(tanggal.year, tanggal.month, tanggal.day, waktu.hour, waktu.minute, waktu.second, waktu.timezone, lokasi1.longitude_dec)
-data_matahari = Sun(T.T_td, T.LST_nampak, lokasi1.latitude_dec)
-data_bulan = Moon(T.T_td, T.LST_nampak, lokasi1.latitude_dec, data_matahari.alpha_matahari, data_matahari.delta_matahari)
+
+koreksi_bulan = MoonCorrection(T.T_td)
+
+print(f'''
+{koreksi_bulan.koreksi_bujur_bulan}
+{koreksi_bulan.lintang_bulan}
+{koreksi_bulan.jarak_bumi_bulan}
+'''
+)
 
 
-print(f"""
-Data Bulan
-Lokasi                  : {lokasi1.nama_lokasi}, {lokasi1.koordinat}
-Bujur rata-rata bulan   : {degrees(data_bulan.bujur_rata_rata_bulan)}°  
-                        : {data_bulan.bujur_rata_rata_bulan} rads                         
-Koreksi bujur bulan     : {data_bulan.koreksi_bujur_bulan} rads
-Bujur Bulan             : {data_bulan.bujur_bulan}° 
-Koreksi Delta Psi       : {data_bulan.delta_psi}°
-Bujur Bulan Nammpak     : {data_bulan.bujur_bulan_nampak}°
+# data_matahari = Sun(T.T_td, T.LST_nampak, lokasi1.latitude_dec)
+# data_bulan = Moon(T.T_td, T.LST_nampak, lokasi1.latitude_dec, data_matahari.alpha_matahari, data_matahari.delta_matahari)
 
-Lintang Bulan           : {data_bulan.lintang_bulan}°
-Jarak Bulan-Bumi        : {data_bulan.jarak_bumi_bulan} km
-Sudut Parallaks         : {data_bulan.sudut_parallaks}°
-Sudut jari-jari bulan   : {data_bulan.sudut_jari_jari_bulan}°
-Alpha                   : {data_bulan.alpha}°
-Delta                   : {data_bulan.delta}°
-Hour Angle              : {data_bulan.hour_angle}°
-Azimuth dari Selatan    : {data_bulan.azimut_selatan}°
-Azimuth                 : {data_bulan.azimut_bulan}°
-Altitude                : {data_bulan.altitude}°
-Sudut Fai               : {data_bulan.sudut_fai}°
-Sudut Fase (i)          : {data_bulan.sudut_fase}°
-Illuminasi Bulan        : {data_bulan.illuminasi}°
 
-DATA MATAHARI
-Theta_terkoreksi        : {data_matahari.theta_terkoreksi}°
-Delta Psi               : {data_matahari.delta_psi}°
-Koreksi aberasi         : {data_matahari.koreksi_aberasi}
-Bujur matahari nampak   : {data_matahari.bujur_matahari_nampak}
-Lintang matahari nampak : {data_matahari.lintang_matahari_tampak}
-Jarak bumi-matahari =   : {data_matahari.jarak_bumi_matahari} ({data_matahari.jarak_bumi_matahari*149598000}km)
-Sudut jari-jari matahari: {data_matahari.sudut_jari_jari_matahari}
-Alpha                   : {data_matahari.alpha_matahari}
-Delta                   : {data_matahari.delta_matahari}
-Hour Angle              : {data_matahari.hour_angle_matahari}
-Azimuth dari selatan    : {data_matahari.azimut_selatan}
-Azimuth                 : {data_matahari.azimut_matahari}
-Altitude                : {data_matahari.altitude}
-Sudut paralaks matahari : {data_matahari.sudut_parallaks_matahari}
+# print(f"""
+# Data Bulan
+# Lokasi                  : {lokasi1.nama_lokasi}, {lokasi1.koordinat}
+# Bujur rata-rata bulan   : {degrees(data_bulan.bujur_rata_rata_bulan)}°  
+#                         : {data_bulan.bujur_rata_rata_bulan} rads                         
+# Koreksi bujur bulan     : {data_bulan.koreksi_bujur_bulan} rads
+# Bujur Bulan             : {data_bulan.bujur_bulan}° 
+# Koreksi Delta Psi       : {data_bulan.delta_psi}°
+# Bujur Bulan Nammpak     : {data_bulan.bujur_bulan_nampak}°
 
-""")
+# Lintang Bulan           : {data_bulan.lintang_bulan}°
+# Jarak Bulan-Bumi        : {data_bulan.jarak_bumi_bulan} km
+# Sudut Parallaks         : {data_bulan.sudut_parallaks}°
+# Sudut jari-jari bulan   : {data_bulan.sudut_jari_jari_bulan}°
+# Alpha                   : {data_bulan.alpha}°
+# Delta                   : {data_bulan.delta}°
+# Hour Angle              : {data_bulan.hour_angle}°
+# Azimuth dari Selatan    : {data_bulan.azimut_selatan}°
+# Azimuth                 : {data_bulan.azimut_bulan}°
+# Altitude                : {data_bulan.altitude}°
+# Sudut Fai               : {data_bulan.sudut_fai}°
+# Sudut Fase (i)          : {data_bulan.sudut_fase}°
+# Illuminasi Bulan        : {data_bulan.illuminasi}°
+
+# DATA MATAHARI
+# Theta_terkoreksi        : {data_matahari.theta_terkoreksi}°
+# Delta Psi               : {data_matahari.delta_psi}°
+# Koreksi aberasi         : {data_matahari.koreksi_aberasi}
+# Bujur matahari nampak   : {data_matahari.bujur_matahari_nampak}
+# Lintang matahari nampak : {data_matahari.lintang_matahari_tampak}
+# Jarak bumi-matahari =   : {data_matahari.jarak_bumi_matahari} ({data_matahari.jarak_bumi_matahari*149598000}km)
+# Sudut jari-jari matahari: {data_matahari.sudut_jari_jari_matahari}
+# Alpha                   : {data_matahari.alpha_matahari}
+# Delta                   : {data_matahari.delta_matahari}
+# Hour Angle              : {data_matahari.hour_angle_matahari}
+# Azimuth dari selatan    : {data_matahari.azimut_selatan}
+# Azimuth                 : {data_matahari.azimut_matahari}
+# Altitude                : {data_matahari.altitude}
+# Sudut paralaks matahari : {data_matahari.sudut_parallaks_matahari}
+
+# """)
 
 
